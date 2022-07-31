4a7c45b6bf05e8213daaa7b323c7c2cdf2245701
oldboyprq <67952647+oldboyprq@users.noreply.github.com>
2022-07-31 18:13:08 +0800

    Update proxy.py

--- proxy.py
+++ proxy.py
@@ -8,7 +8,6 @@ for i in proxy:
         with open('{}.txt'.format(i), 'w', encoding='utf-8') as f:
             r = requests.get("https://bitbucket.org/huwo1/proxy_nodes/raw/f31ca9ec67b84071515729ff45b011b6b09c10f2/{}.md".format(i))
             data = r.text
-            print(data)
             f.write(data)
             f.close()
     except Exception as e:
