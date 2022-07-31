ade797d65e733d94320ddeeb4fcae9a11077c876
oldboyprq <67952647+oldboyprq@users.noreply.github.com>
2022-07-31 18:42:25 +0800

    Update proxy.py

--- proxy.py
+++ proxy.py
@@ -2,22 +2,16 @@ import requests
 
 proxy = ['proxy', 'vmess', 'ss', 'trojan', 'clash']
 for i in proxy:
-    if i != 'clash':
-        try:
-            with open('{}.txt'.format(i), 'w', encoding='utf-8') as f:
+    try:
+        with open('{}.txt'.format(i), 'w', encoding='utf-8') as f:
+            if i != 'clash':
                 r = requests.get("https://bitbucket.org/huwo1/proxy_nodes/raw/f31ca9ec67b84071515729ff45b011b6b09c10f2/{}.md".format(i))
-                data = r.text
-                f.write(data)
-                f.close()
-        except Exception as e:
-            print(e)
-    else:
-        try:
-            with open('clash.yaml'.format(i), 'w', encoding='utf-8') as f:
-                r = requests.get("https://bitbucket.org/huwo1/proxy_nodes/raw/f31ca9ec67b84071515729ff45b011b6b09c10f2/clash.yaml")
+            else:
+                r = requests.get("https://bitbucket.org/huwo1/proxy_nodes/raw/f31ca9ec67b84071515729ff45b011b6b09c10f2/{}.yaml".format(i))
                 r.encoding = r.apparent_encoding
-                data = r.text
-                f.write(data)
-                f.close()
-        except Exception as e:
-            print(e)
+            data = r.text
+            f.write(data)
+            f.close()
+    except Exception as e:
+        print(e)
+
