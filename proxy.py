5e81c8ef089f5afd6b4b71fa0d951bc1968688b9
oldboyprq <67952647+oldboyprq@users.noreply.github.com>
2022-07-31 18:25:12 +0800

    Update proxy.py

--- proxy.py
+++ proxy.py
@@ -1,14 +1,20 @@
-import requests
-from lxml import html
-
-
-proxy = ['proxy', 'vmess', 'ss', 'trojan']
+proxy = ['proxy', 'vmess', 'ss', 'trojan', 'clash']
 for i in proxy:
-    try:
-        with open('{}.txt'.format(i), 'w', encoding='utf-8') as f:
-            r = requests.get("https://bitbucket.org/huwo1/proxy_nodes/raw/f31ca9ec67b84071515729ff45b011b6b09c10f2/{}.md".format(i))
-            data = r.text
-            f.write(data)
-            f.close()
-    except Exception as e:
-        print(e)
+    if i != 'clash':
+        try:
+            with open('{}.txt'.format(i), 'w', encoding='utf-8') as f:
+                r = requests.get("https://bitbucket.org/huwo1/proxy_nodes/raw/f31ca9ec67b84071515729ff45b011b6b09c10f2/{}.md".format(i))
+                data = r.text
+                f.write(data)
+                f.close()
+        except Exception as e:
+            print(e)
+    else:
+        try:
+            with open('clash.yaml'.format(i), 'w', encoding='utf-8') as f:
+                r = requests.get("https://bitbucket.org/huwo1/proxy_nodes/raw/f31ca9ec67b84071515729ff45b011b6b09c10f2/clash.yaml")
+                data = r.text
+                f.write(data)
+                f.close()
+        except Exception as e:
+            print(e)
