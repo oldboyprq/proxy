c19362e7bab7e065cbe3fe3ee096be0e90f3c9a1
oldboyprq <67952647+oldboyprq@users.noreply.github.com>
2022-07-31 18:10:37 +0800

    Update proxy.py

--- proxy.py
+++ proxy.py
@@ -6,9 +6,9 @@ proxy = ['proxy', 'vmess', 'ss', 'trojan']
 for i in proxy:
     try:
         with open('{}.txt'.format(i), 'w', encoding='utf-8') as f:
-            r = requests.get("https://github.com/colatiger/v2ray-nodes/blob/master/{}.md".format(i))
-            tree = html.fromstring(r.text)
-            data = tree.xpath("//*[@id='readme']/article/p/text()")[0]
+            r = requests.get("https://bitbucket.org/huwo1/proxy_nodes/raw/f31ca9ec67b84071515729ff45b011b6b09c10f2/{}.md".format(i))
+            data = r.text
+            print(data)
             f.write(data)
             f.close()
     except Exception as e:
